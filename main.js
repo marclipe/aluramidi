e80eade1946af1208cd547103f3d52dd1abce148
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2022-12-30 18:27:06 -0300

    dynamic id javascript

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play(); 
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play(); 
 }
 
 // document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); 
 
-listaDeTeclas[0].onclick = tocaSomPom()
+let contador = 0; 
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //'#som_(instrumento)' template string 
+    const idAudio =  `#som_${instrumento}`;
+    // console.log(idAudio)
+
+    tecla.onclick = function() {
+        tocaSom(idAudio)
+    }; 
+
+    contador = contador + 1; 
+
+    // console.log(contador);
+}
